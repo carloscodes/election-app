491ebe575a6bf67872e5e6c58a3f2be3eee36493
unknown <chavarriacarlos18@gmail.com>
2019-08-11 13:39:36 -0700

    added elections, needs better way to pull data though

--- main.js
+++ main.js
@@ -4,6 +4,72 @@
  * @param {function(Object)} callback Function which takes the
  *     response object as a parameter.
  */
+const electionsCA = [
+  {
+    id: 4834,
+    name: 'Los Angeles City Council District 12 Runoff Election',
+    electionDay: '2019-08-13',
+    ocdDivisionId: 'ocd-division/country:us/state:ca/county:los_angeles'
+  },
+  {
+    id: 4838,
+    name: 'San Bernardino County Consolidated Mail Election',
+    electionDay: '2019-08-27',
+    ocdDivisionId: 'ocd-division/country:us/state:ca/county:san_bernardino'
+  }
+];
+
+const electionMN = [
+  {
+    id: 4835,
+    name: 'Minnesota Primary Election',
+    electionDay: '2019-08-13',
+    ocdDivisionId: 'ocd-division/country:us/state:mn'
+  }
+];
+
+const electionOK = [
+  {
+    id: 4836,
+    name: 'Oklahoma Special Election',
+    electionDay: '2019-08-13',
+    ocdDivisionId: 'ocd-division/country:us/state:ok'
+  }
+];
+
+const electionUT = [
+  {
+    id: '4837',
+    name: 'Utah Municipal Primary Election',
+    electionDay: '2019-08-13',
+    ocdDivisionId: 'ocd-division/country:us/state:ut'
+  }
+];
+
+const electionAL = [
+  {
+    id: 4841,
+    name: 'City of Talladega and City of Montgomery Municipal Election',
+    electionDay: '2019-08-27',
+    ocdDivisionId: 'ocd-division/country:us/state:al'
+  },
+  {
+    id: 4856,
+    name: 'Alabama State House District 42 Special Primary Election',
+    electionDay: '2019-08-20',
+    ocdDivisionId: 'ocd-division/country:us/state:al/sldl:42'
+  }
+];
+
+const electionNC = [
+  {
+    id: 4845,
+    name: 'North Carolina Primary Election',
+    electionDay: '2019-09-10',
+    ocdDivisionId: 'ocd-division/country:us/state:nc'
+  }
+];
+
 function lookup(address, callback) {
   /**
    * Election ID for which to fetch voter info.
@@ -20,6 +86,67 @@ function lookup(address, callback) {
     params: { electionId: electionId, address: address }
   });
   req.execute(callback);
+}
+
+function checkElectionsForMatch(addrs) {
+  let e = document.getElementById('elections');
+  let electionsFound = '';
+  let len = 0;
+
+  addrs.forEach(data => {
+    if (data.long_name === 'California' || data.short_name === 'CA') {
+      len = electionsCA.length;
+      for (let i = 0; i < len; i++) {
+        electionsFound += `<h3>${electionsCA[i].name} ${
+          electionsCA[i].electionDay
+        } <h3>`;
+      }
+      e.innerHTML = `<h2>Upcoming elections around you: ${electionsFound}</h2>`;
+    } else if (data.long_name === 'Alabama' || data.short_name === 'AL') {
+      len = electionsAL.length;
+      for (let i = 0; i < len; i++) {
+        electionsFound += `<h3>${electionsAL[i].name} ${
+          electionsAL[i].electionDay
+        } <h3>`;
+      }
+      e.innerHTML = `<h2>Upcoming elections around you: ${electionsFound}</h2>`;
+    } else if (
+      data.long_name === 'North Carolina' ||
+      data.short_name === 'NC'
+    ) {
+      len = electionsNC.length;
+      for (let i = 0; i < len; i++) {
+        electionsFound += `<h3>${electionsNC[i].name} ${
+          electionsNC[i].electionDay
+        } <h3>`;
+      }
+      e.innerHTML = `<h2>Upcoming elections around you: ${electionsFound}</h2>`;
+    } else if (data.long_name === 'Utah' || data.short_name === 'UT') {
+      len = electionsUT.length;
+      for (let i = 0; i < len; i++) {
+        electionsFound += `<h3>${electionsUT[i].name} ${
+          electionsUT[i].electionDay
+        } <h3>`;
+      }
+      e.innerHTML = `<h2>Upcoming elections around you: ${electionsFound}</h2>`;
+    } else if (data.long_name === 'Minnesota' || data.short_name === 'MN') {
+      len = electionsMN.length;
+      for (let i = 0; i < len; i++) {
+        electionsFound += `<h3>${electionsMN[i].name} ${
+          electionsMN[i].electionDay
+        } <h3>`;
+      }
+      e.innerHTML = `<h2>Upcoming elections around you: </h2> ${electionsFound}`;
+    } else if (data.long_name === 'Oklahoma' || data.short_name === 'OK') {
+      len = electionsOK.length;
+      for (let i = 0; i < len; i++) {
+        electionsFound += `<h3>${electionsOK[i].name} ${
+          electionsOK[i].electionDay
+        } <h3>`;
+      }
+      e.innerHTML = `<h2>Upcoming elections around you: ${electionsFound}</h2>`;
+    }
+  });
 }
 
 function getRepresentatives(address, callback) {
@@ -212,6 +339,8 @@ takeAddress = () => {
   }
 
   console.log(s);
+
+  checkElectionsForMatch(s);
   /**
    * Initialize the API client and make a request.
    */
@@ -219,10 +348,6 @@ takeAddress = () => {
   //lookup(`${s}`, renderResults);
   getRepresentatives(s, reps);
 };
-
-// enter button
-const s = document.getElementById('vals');
-s.addEventListener('onclick', takeAddress);
 
 getLocation = () => {
   var options = {
@@ -246,6 +371,10 @@ getLocation = () => {
       if (status === 'OK') {
         if (results[0]) {
           address = results[0].formatted_address;
+
+          let addrs = results[0].address_components;
+
+          checkElectionsForMatch(addrs);
 
           userInput.value = address;
           gapi.client.setApiKey('AIzaSyDMaos2Ppqe76FRC6cII_k-oC2gi89MeUc');
